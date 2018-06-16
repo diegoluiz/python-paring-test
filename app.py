7b91544b71180a75ac5a4a2ba8227a1dad8d921a
Diego Luiz <diegorcl@gmail.com>
2018-06-16 21:35:21 +0100

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
 # http://flask.pocoo.org/docs/1.0/quickstart/
 
-from flask import Flask
+from flask import Flask, request, make_response
+
 app = Flask(__name__)
 
+people = []
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user')
+def show_users():
+    return "\n".join(people)
+
+@app.route('/user/<username>', methods=['GET'])
+def show_user_profile(username):
+    if username in people:
+        return 'Hi %s, You are in the list' % username
+    else:
+        return '%s, you are not in the list' % username
+
+# curl -v -XPOST 127.0.0.1:5000/user -H 'content-type: application/json' -d '{ "name": "renata" }' 
+@app.route('/user', methods=['POST'])
+def add_user_profile():
+    user = request.get_json()
+    username = user['name']
+    
+    if username in people:
+        return 'Hi %s, you are in the list! :)' % username
+    else:
+        people.append(username)
+        return '%s, you were not in the list but you\'re just added' % username 
+
+@app.route('/user/<username>', methods=['DELETE'])
+def remove_user_profile(username):
+    if username in people:
+        people.remove(username)
+        return '%s, you were removed from the list.' % username
+    else:
+        return '%s, you were not in the list.' % username
 
 # $ export FLASK_APP=app.py
 # $ flask run
